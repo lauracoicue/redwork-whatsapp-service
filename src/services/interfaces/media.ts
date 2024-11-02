41a2b0a8034cf035448c96bb472a14d16534b22a
code3743 <jota.lopez@correounivalle.edu.co>
2024-11-02 15:27:55 -0500

    Remove Media interface and introduce new WhatsApp service types for message handling

--- src/services/interfaces/media.ts
+++
@@ -1,7 +0,0 @@
-interface Media {
-    base64: string;
-    mimetype: string;
-    caption: string | undefined;
-}
-
-export default Media;
---
+++ src/services/interfaces/whatsapp-servive-types.ts
@@ -0,0 +1,21 @@
+interface MessageRequest {
+    phone: string;
+    country: string;
+    message: string | LocationMessage  | MediaMessage;
+    userType: 'worker' | 'user'; 
+}
+
+
+interface LocationMessage {
+    latitude: number;
+    longitude: number;
+}
+
+interface MediaMessage {
+    base64?: string;
+    url?: string;
+    mimetype?: string;
+    caption?: string;
+}
+
+export {MessageRequest, LocationMessage, MediaMessage};
