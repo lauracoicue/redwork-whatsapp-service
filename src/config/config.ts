57f98eea3d24cfa613efe4e1c0fd549efcd00e76
code3743 <jota.lopez@correounivalle.edu.co>
2024-11-16 07:57:31 -0500

    Update environment variables in .env.example and add configuration exports for HOST_BACKEND, HOST_FRONTEND and HOST_SERVICE in config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,4 @@
 export const  lastMessageTimeout = 120;
+export const hostBackend = process.env.HOST_BACKEND || 'http://localhost:3000';
+export const hostFrontend = process.env.HOST_FRONTEND || 'http://localhost:4321';
+export const hostService = process.env.HOST_SERVICE || 'http://localhost:3000';
