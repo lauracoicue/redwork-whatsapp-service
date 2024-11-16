69cb29798af516d82ff40e53fc5b71665a3c50d6
code3743 <jota.lopez@correounivalle.edu.co>
2024-11-16 07:42:45 -0500

    Refactor registerController to improve error handling and streamline password setting logic

--- src/modules/api/controllers/controllers.ts
+++ src/modules/api/controllers/controllers.ts
@@ -1,8 +1,9 @@
 import { Request, Response } from 'express';
 import {WhatsappStatusService, userWhatsappService, adminWhatsappService} from '../../../services/whatsapp-service';
-import { parsePhoneNumber } from '../../../utils/number-paser';
+import { normalizePhoneNumber, parsePhoneNumber } from '../../../utils/number-paser';
 import { MessageRequest } from '../../../services/interfaces/whatsapp-servive-types';
 import registerModule from '../../register/register';
+import Worker from '../../../models/worker';
 
 const sendMessageController = async (req: Request, res: Response) => {  
     try {
@@ -70,79 +71,90 @@ const securityPasswordController = async (req: Request, res: Response) => {
 };
 
 
+const renderError = (res: Response, title: string, message: string, link: string) => {
+    res.status(400).render('error', { title, message, link });
+};
+
+const renderSecurityPassword = (res: Response, params:{title: string, message: string, link: string | undefined, action: string | undefined, id: string | undefined}) => {
+    console.log(params);
+    res.status(200).render('security-password', { ...params });
+};
+
+const isRequestExpired = (createdAt: Date, expirationTime: number = 5 * 60 * 1000) => {
+    return new Date().getTime() - createdAt.getTime() > expirationTime;
+};
+
 const registerController = async (req: Request, res: Response) => {
     try {
+        const phone = req.method === 'GET' ? req.query.id as string : req.body.id as string;
+        const password = req.method === 'POST' ? req.body.password : undefined;
+
+        if (!phone) {
+            renderError(res, 'Error al establecer contraseña', 'El número de teléfono es requerido', 'http://localhost:3000/');
+            return;
+        }
+
+        const register = registerModule.pendingRegister[phone];
+
         if (req.method === 'GET') {
-            const phone = req.query.id as string;
-          res.render('security-password', {
-              title: 'Finaliza tu registro',
-              message: 'Por favor establece una contraseña para finalizar tu registro',
-              action: '/api/register',
-              id: phone,
-          });
-          return;
-          }
-      
-          const password = req.body.password;
-          const phone = req.body.id;
+            const worker = await Worker.findOne({ where: { phone: normalizePhoneNumber(phone).phone } });
 
-          if (!password) {
-              res.status(400).render('error', {
-                  title: 'Error al establecer contraseña',
-                  message: 'La contraseña es requerida',
-                  link: 'http://localhost:3000/'
-              });
-              return;
-          }
-        
-          if (!phone) {
-              res.status(400).render('error', {
-                  title: 'Error al establecer contraseña',
-                  message: 'El número de teléfono es requerido',
-                  link: 'http://localhost:3000/'
-              });
-              return;
-          }
-
-            if (!registerModule.pendingRegister[phone]) {
-                res.status(400).render('error', {
-                    title: 'Error al establecer contraseña',
-                    message: 'El número de teléfono no se encuentra registrado',
-                    link: 'http://localhost:3000/'
+            if (worker) {
+                renderSecurityPassword(res,{
+                     title:'Usuario registrado', message: `El usuario ${worker.name} ya se encuentra registrado`, link: 'http://localhost:3000/',
+                     id: undefined, action: undefined
                 });
                 return;
             }
 
+            if (register && isRequestExpired(register.createdAt)) {
+                renderError(res, 'Error al establecer contraseña', 'El tiempo para establecer la contraseña ha expirado, por favor vuelva a escribir', 'https:wa.me/573002222222');
+                return;
+            }
 
-          const now = new Date();
-          const createAt = registerModule.pendingRegister[phone].createdAt
-      
-          if (now.getTime() - createAt.getTime() > 5 * 60 * 1000) {
-              res.status(400).render('error', {
-                  title: 'Error al establecer contraseña',
-                  message: 'El tiempo para establecer la contraseña ha expirado, por favor vuelva a escribir',
-                  link: 'https:wa.me/573002222222'
-              });
-              return;
-          }
-      
-      
-          registerModule.setPasswordByPhone(phone, password);
-      
-          res.status(200).render('security-password', {
-              title: 'Contraseña establecida',
-              message: 'La contraseña ha sido establecida correctamente',
-              link: 'https://wa.me/573002214787?text=Contraseña%20establecida',
-              action: undefined,
-          })
-      
+            if (register && register.active){
+                renderSecurityPassword(res, {
+                    title: 'Ya se ha establecido la contraseña', message: 'Ya se ha establecido la contraseña para este número de teléfono', link: 'http://localhost:3000/', id: undefined, action: undefined});
+            }
+
+            renderSecurityPassword(res, {
+                title: 'Establecer contraseña',
+                message: 'Por favor establezca una contraseña para acceder a la aplicación',
+                link: undefined,
+                action: '/api/register',
+                id: phone,
+            });
+            return;
+        }
+
+        if (req.method === 'POST') {
+            if (!password) {
+                renderError(res, 'Error al establecer contraseña', 'La contraseña es requerida', 'http://localhost:3000/');
+                return;
+            }
+
+            if (!register) {
+                renderError(res, 'Error al establecer contraseña', 'El número de teléfono no se encuentra registrado', 'http://localhost:3000/');
+                return;
+            }
+
+            if (isRequestExpired(register.createdAt)) {
+                renderError(res, 'Error al establecer contraseña', 'El tiempo para establecer la contraseña ha expirado, por favor vuelva a escribir', 'https:wa.me/573002222222');
+                return;
+            }
+
+            registerModule.setPasswordByPhone(phone, password);
+            renderSecurityPassword(res, {
+                title:'Contraseña establecida', message:'La contraseña ha sido establecida correctamente',link: 'https://wa.me/573002214787?text=Contraseña%20establecida',
+                id: undefined, action: undefined
+            });
+            return;
+        }
     } catch (error) {
-        res.status(500).send({
-            message: `Error registering user: ${error}`,
-        });
+        res.status(500).send({ message: `Error registering user: ${error}` });
     }
-   
 };
+
 
 
 export { sendMessageController, statusServiceController, securityPasswordController, registerController };
