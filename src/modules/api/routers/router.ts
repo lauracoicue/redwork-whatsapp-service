99cd8310b5d0ead917af237a227878f34327d38a
Esmeralda-RG <esmeralda.rivas@correounivalle.edu.co>
2024-12-08 19:48:03 -0500

    feat: Add urlPasswordController to handle password reset requests and update router

--- src/modules/api/routers/router.ts
+++ src/modules/api/routers/router.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerController, securityPasswordController, sendMessageController, statusServiceController, deleteAccountController } from "../controllers/controllers";
+import { registerController, securityPasswordController, sendMessageController, statusServiceController, deleteAccountController, urlPasswordController} from "../controllers/controllers";
 
 
 
@@ -9,6 +9,7 @@ const router = Router();
 router.get('/status', statusServiceController); 
 router.post('/send-message', sendMessageController);
 router.get('/security-password',securityPasswordController);
+router.post('/url-password',urlPasswordController);
 router.get('/register', registerController);
 router.post('/delete-account', deleteAccountController);
 router.post('/register', registerController);
