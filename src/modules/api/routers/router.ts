b323748cb7854636406c70b65fc1256b0c17f4a1
code3743 <jota.lopez@correounivalle.edu.co>
2024-12-08 21:27:55 -0500

    feat: Renombrar ruta de 'url-password' a 'reset-password' en el enrutador

--- src/modules/api/routers/router.ts
+++ src/modules/api/routers/router.ts
@@ -9,10 +9,9 @@ const router = Router();
 router.get('/status', statusServiceController); 
 router.post('/send-message', sendMessageController);
 router.get('/security-password',securityPasswordController);
-router.post('/url-password',urlPasswordController);
+router.post('/reset-password',urlPasswordController);
 router.get('/register', registerController);
 router.post('/delete-account', deleteAccountController);
-router.post('/register', registerController);
 
 
 
